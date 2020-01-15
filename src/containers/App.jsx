3ff2fa3dc6c4fff3eb35a2651bd41159dc222d35
unknown <dracsharp@gmail.com>
2020-01-15 16:44:54 +0000

    error check + github pages

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
+import ErrorBoundry from "../components/Errorboundry"
 
 // const App = () => {
 //     return (
@@ -46,7 +47,9 @@ class App extends Component {
       <h1 className="title">ROBOFRIENDS</h1>
       <SearchBox searchChange={this.onSearchChange}/>
       <Scroll>
-       <CardList robots={filteredRobots}/>
+        <ErrorBoundry>
+          <CardList robots={filteredRobots}/>
+        </ErrorBoundry>
       </Scroll>
        </div>
   )
