abf70f32f97c7dd434698dac84617f1f55805091
unknown <dracsharp@gmail.com>
2020-01-15 16:45:08 +0000

    Updates

---
+++ precache-manifest.e00cef8ebd1fd19c2a954247154b642b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c26faa71f112247fb89cbe80d39143d0",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "88bae5fcb27fcad9d35b",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "b0013f89a49660900c5a",
+    "url": "/robofriends/static/css/main.504fec2e.chunk.css"
+  },
+  {
+    "revision": "88bae5fcb27fcad9d35b",
+    "url": "/robofriends/static/js/2.2475d6f8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends/static/js/2.2475d6f8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b0013f89a49660900c5a",
+    "url": "/robofriends/static/js/main.ef81d4b8.chunk.js"
+  },
+  {
+    "revision": "d6ceab57e5523008bfa1",
+    "url": "/robofriends/static/js/runtime-main.03e2f00f.js"
+  }
+]);
